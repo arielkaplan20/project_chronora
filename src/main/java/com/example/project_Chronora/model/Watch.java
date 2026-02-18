90b58feee967ddcfa572df038ec18ff6cc264108
ariel <ariel.kaplan011@gmail.com>
2026-02-18 21:04:42 +0200

    with data

--- src/main/java/com/example/project_Chronora/model/Watch.java
+++ src/main/java/com/example/project_Chronora/model/Watch.java
@@ -11,7 +11,7 @@ public class Watch implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY
     private Long id;
 
     @NotBlank
